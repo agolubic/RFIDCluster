37f37a94203487e1b1bf21e486b255044e13fabc
agolubic86 <agolubic86@gmail.com>
2018-12-16 18:10:36 +0100

    Add default package name

--- mysql_cluster_node.sh
+++ mysql_cluster_node.sh
@@ -7,6 +7,7 @@ GREEN='\033[1;32m'
 NC='\033[0m'
 
 MSC_DATA_NODE_URL=https://dev.mysql.com/get/Downloads/MySQL-Cluster-7.6/mysql-cluster-community-data-node_7.6.6-1ubuntu18.04_amd64.deb
+MSC_FILE_NAME='mysql_cluster_data_node.deb'
 DEPENDENCY=libclass-methodmaker-perl
 CONF_FILE=/etc/my.cnf
 NDBM_HOST=46.101.248.77
@@ -71,7 +72,7 @@ done
 #DOWNLOAD PACKAGE
 echo ${GREEN}Downloading MYSQL Cluster Data Node ...${NC}
 cd ~
-wget $MSC_DATA_NODE_URL
+wget $MSC_DATA_NODE_URL -O $MSC_FILE_NAME
 
 #INSTALL PREREQUISITES
 echo ${GREEN}Installing dependency ...${NC}
@@ -85,7 +86,7 @@ fi
 
 #INSTALL PACKAGE
 echo ${GREEN}Installing package ...${NC}
-sudo dpkg -i mysql-cluster-community-data-node_7.6.6-1ubuntu18.04_amd64.deb
+sudo dpkg -i $MSC_FILE_NAME
 
 
 #CREATE CONFIG FILE
